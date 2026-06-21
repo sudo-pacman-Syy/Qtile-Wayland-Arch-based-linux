bcf2506d71936a089f71c4aa774fcd280746edea
sudo-pacman-Syy <qspawn.789@gmail.com>
2026-06-21 15:50:11 +0400

    Install wl-clipboard and cliphist
    Added installation of wl-clipboard and cliphist.

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ mkdir -p ~/.config/fastfetch
 mkdir -p ~/Pictures/Screenshots
 mkdir -p ~/.config/mako
 sudo pacman -S --needed grim slurp swaybg --noconfirm
+sudo pacman -S wl-clipboard
+sudo pacman -S cliphist
 
 cp -v config/qtile/config.py ~/.config/qtile/
 cp -v config/alacritty/alacritty.toml ~/.config/alacritty/ 2>/dev/null || echo "Alacritty config not found, skipping..."
